4a7a81a5c4a498d24c6c17d729811e4106e0d313
DragonTheCoder <77131328+DragonTheCoder@users.noreply.github.com>
2021-01-08 02:21:57 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("fantasy", {
     name: "fantasy", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "f", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +8,8 @@ addLayer("fantasy", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Imagination", // Name of prestige currency
+    baseResource: "fantasy points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,7 +22,7 @@ addLayer("fantasy", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "f", description: "F: Reset for imagination points", onPress(F){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
